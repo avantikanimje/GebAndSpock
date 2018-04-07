82e2e07973760c0f36cafc14dde6ea165ef34c86
animje <animje@qti.qualcomm.com>
2018-04-07 12:22:22 +0530

    Initial commit

---
+++ src/test/groovy/specs/GebishOrgSpec.groovy
@@ -0,0 +1,26 @@
+package specs
+
+import geb.spock.GebSpec
+import pages.GebishOrgHomePage
+import pages.TheBookOfGebPage
+import modules.ManualsMenuModule
+
+class GebishOrgSpec extends GebSpec {
+
+    def "can get to the current Book of Geb"() {
+        when:
+        to GebishOrgHomePage
+
+        and:
+        manualsMenu.open()
+
+        then:
+        manualsMenu.links[0].text().startsWith("current")
+
+        when:
+        manualsMenu.links[0].click()
+
+        then:
+        at TheBookOfGebPage
+    }
+}
